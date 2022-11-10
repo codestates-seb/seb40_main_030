f2c9e06eb23188f3eb3dd8dcafa20b8e2c9f4918
leezer94 <2kunhee94@gmail.com>
2022-11-10 23:08:22 +0900

    프론트엔드 초기 개발환경 설정

---
+++ frontend/prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  endOfLine: 'lf',
+};
