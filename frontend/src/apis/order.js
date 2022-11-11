a933c19278ef65ee6f3b7632753ed1193e728c57
leezer94 <2kunhee94@gmail.com>
2022-11-11 15:45:01 +0900

    Feat : 1차 ERD 참고 order 관련 MSW rest handler 생성

---
+++ frontend/src/apis/order.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.REACT_APP_BASE_URL,
+});
+
+// 전체 주문 조회
+const getAllOrders = async () => {
+  const { data } = await apiClient.get('/api/orders');
+
+  return data;
+};
+
+// 단일 주문 조회
+const getOrderById = async (orderId) => {
+  const { data } = await apiClient.get(`/api/orders/${orderId}`);
+
+  console.log(data);
+
+  return data;
+};
+
+// 주문 요청
+const createNewOrder = async (newOrder) => {
+  return await apiClient.post('/api/orders', newOrder);
+};
+
+// 주문 수정
+const modifyOrder = async (orderId, modifiedOrder) => {
+  const { data } = await apiClient.patch(
+    `/api/orders/${orderId}`,
+    modifiedOrder
+  );
+
+  return data;
+};
+
+// 주문 취소
+const deleteOrder = async (orderId) => {
+  return await apiClient.delete(`/api/orders/${orderId}`);
+};
+
+export { getAllOrders, getOrderById, createNewOrder, modifyOrder, deleteOrder };
