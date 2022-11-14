b459b1d232a695cb38dcc93b4f7fd6a5e9aac1c0
Dongjin Kim <nyong9221@gmail.com>
2022-11-14 19:17:00 +0900

    Docs: getOrder, getOrders 테스트 코드 작성

--- backend/src/test/java/backend/domain/order/controller/OrderControllerTest.java
+++ backend/src/test/java/backend/domain/order/controller/OrderControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -27,8 +26,7 @@ import static backend.util.ApiDocumentUtils.getResponsePreProcessor;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -44,6 +42,7 @@ public class OrderControllerTest {
 
     @Autowired
     private OrderRepository orderRepository;
+
 
     @Test @DisplayName("Order 생성")
     void postOrder_Test() throws Exception {
@@ -86,6 +85,7 @@ public class OrderControllerTest {
                         )
                 ));
     }
+
 
     @Test @DisplayName("Order 수정")
     void patchOrder_Test() throws Exception {
@@ -143,6 +143,7 @@ public class OrderControllerTest {
                 ));
     }
 
+
     @Test @DisplayName("Order 삭제")
     void deleteOrder () throws Exception {
         // given
@@ -165,6 +166,86 @@ public class OrderControllerTest {
                         responseFields(
                                 Arrays.asList(
                                         fieldWithPath("data").type(JsonFieldType.STRING).description("Api 성공 메시지")
+                                )
+                        )
+                ));
+    }
+
+
+    @Test @DisplayName("Order 조회")
+    void getOrder() throws Exception {
+        // given
+        Long orderId = 103L;
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                get("/orders/{orderId}", orderId)
+        );
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andDo(document("getOrder",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("orderId").description("Order 식별자")
+                        ),
+                        responseFields(
+                                Arrays.asList(
+                                        fieldWithPath("orderId").type(JsonFieldType.NUMBER).description("Order 식별자"),
+                                        fieldWithPath("status").type(JsonFieldType.STRING).description("Order 상태"),
+                                        fieldWithPath("startTime").type(JsonFieldType.STRING).description("Order 시작일자"),
+                                        fieldWithPath("endTime").type(JsonFieldType.STRING).description("Order 종료일자"),
+                                        fieldWithPath("createdAt").type(JsonFieldType.STRING).description("Order 생성일자"),
+                                        fieldWithPath("modifiedAt").type(JsonFieldType.STRING).description("Order 수정일자")
+                                )
+                        )
+                        ));
+    }
+
+
+    @Test @DisplayName("Order 전체 조회")
+    void getOrders() throws Exception {
+        // given
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                get("/orders")
+                        .param("page","1")
+                        .param("size","10")
+                        .param("sort","createdAt,desc")
+        );
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andDo(document("getOrders",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        requestParameters(
+                                parameterWithName("page").description("페이지 번호(default = 1)"),
+                                parameterWithName("size").description("페이지 사이즈(default = 10)"),
+                                parameterWithName("sort").description("페이지 정렬(default = createdAt, desc)")
+                        ),
+                        responseFields(
+                                Arrays.asList(
+                                        fieldWithPath("content").type(JsonFieldType.ARRAY).description("Order 페이지"),
+                                        fieldWithPath("content[].id").type(JsonFieldType.NUMBER).description("Order 식별자"),
+                                        fieldWithPath("content[].status").type(JsonFieldType.STRING).description("Order 상태"),
+                                        fieldWithPath("content[].startTime").type(JsonFieldType.STRING).description("Order 시작일자"),
+                                        fieldWithPath("content[].endTime").type(JsonFieldType.STRING).description("Order 종료일자"),
+                                        fieldWithPath("content[].createdAt").type(JsonFieldType.STRING).description("Order 생성일자"),
+                                        fieldWithPath("content[].modifiedAt").type(JsonFieldType.STRING).description("Order 수정일자"),
+
+                                        fieldWithPath("size").type(JsonFieldType.NUMBER).description("Order 페이지 사이즈"),
+                                        fieldWithPath("totalPages").type(JsonFieldType.NUMBER).description("Order 총 페이지 수"),
+                                        fieldWithPath("totalElements").type(JsonFieldType.NUMBER).description("Order 총 데이터 수"),
+                                        fieldWithPath("first").type(JsonFieldType.BOOLEAN).description("Order 첫 페이지 유무"),
+                                        fieldWithPath("last").type(JsonFieldType.BOOLEAN).description("Order 마지막 페이지 유무"),
+                                        fieldWithPath("sorted").type(JsonFieldType.BOOLEAN).description("Order 정렬 유무"),
+                                        fieldWithPath("pageNumber").type(JsonFieldType.NUMBER).description("Order 페이지 번호"),
+                                        fieldWithPath("numberOfElements").type(JsonFieldType.NUMBER).description("Order 페이지 내 데이터 수")
                                 )
                         )
                 ));
