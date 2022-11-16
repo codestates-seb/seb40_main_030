42e0b0908bb4714325126339d93e180a93714692
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2022-11-16 15:30:35 +0900

    Delete BatteryRepository.java

--- backend/src/main/java/backend/domain/battery/repository/BatteryRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.domain.battery.repository;
-
-import backend.domain.battery.entity.Battery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BatteryRepository extends JpaRepository<Battery,Long> {
-}
