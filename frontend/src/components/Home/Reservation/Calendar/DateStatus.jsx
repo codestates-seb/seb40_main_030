e03a9b20bd3b9880dedbc1f1e9a6cbb77a922c91
leezer94 <2kunhee94@gmail.com>
2022-11-18 16:03:57 +0900

    Feat : singleDate / multiDate 예약 세부로직 추가

---
+++ frontend/src/components/Home/Reservation/Calendar/DateStatus.jsx
@@ -0,0 +1,14 @@
+import * as S from './Calendar.style';
+
+const DateStatus = ({ content, date, time }) => {
+  return (
+    <S.ReservationBox>
+      <S.DateStatus>{content}</S.DateStatus>
+      <span>{`${date?.month} 월 ${date?.date} 일 ${time?.hours || 0} 시 ${
+        time?.minutes || 0
+      } 분 ${content === '대여' ? '부터' : '까지'}`}</span>
+    </S.ReservationBox>
+  );
+};
+
+export default DateStatus;
