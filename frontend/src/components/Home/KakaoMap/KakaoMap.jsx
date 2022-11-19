f05986dff0419b137d79539db8a3e56324738ca1
leezer94 <2kunhee94@gmail.com>
2022-11-19 16:53:33 +0900

    Feat : 지도 마우스 위치 이동시 현재위치 동적 업데이트

--- frontend/src/components/Home/KakaoMap/KakaoMap.jsx
+++ frontend/src/components/Home/KakaoMap/KakaoMap.jsx
@@ -1,7 +1,7 @@
 import { Map } from 'react-kakao-maps-sdk';
 import { useState } from 'react';
 import { useGetAllZones, useGetFilteredZone } from './hooks';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import {
   reservationState,
   currentLocationState,
@@ -12,6 +12,7 @@ import MarkerContainer from './MarkerContainer';
 import MapIndicator from './MapIndicator/MapIndicator';
 
 import * as S from './KakaoMap.style';
+import { DEFAULT_LOCATION } from '../../../constants/location';
 
 // type Location = {
 //   location: {
@@ -27,10 +28,11 @@ const KakaoMap = () => {
   const { data: zones, isSuccess } = useGetAllZones();
   const { filteredZones } = useGetFilteredZone();
   const { dateFixed } = useRecoilValue(reservationState);
-  const currentLocation = useRecoilValue(currentLocationState);
+  const [currentLocation, setCurrentLocation] =
+    useRecoilState(currentLocationState);
 
-  const latitude = currentLocation?.latitude || 37.4965;
-  const longitude = currentLocation?.longitude || 127.0248;
+  const latitude = currentLocation?.latitude || DEFAULT_LOCATION.latitude;
+  const longitude = currentLocation?.longitude || DEFAULT_LOCATION.longitude;
 
   // location 기반 필터링시에 범위를 어디까지 할것인가를 알아봐야함
 
@@ -46,6 +48,12 @@ const KakaoMap = () => {
             }}
             isPanto={true}
             style={{ width: '100%', height: '100%' }}
+            onDragEnd={(map) =>
+              setCurrentLocation({
+                latitude: map.getCenter().getLat(),
+                longitude: map.getCenter().getLng(),
+              })
+            }
             level={3}
           >
             {dateFixed.date && dateFixed.time
