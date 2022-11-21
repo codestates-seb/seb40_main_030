e40ec79d46375ace06723ad1e3450503566dfc0c
mingu0429 <mingu0429@gmail.com>
2022-11-21 20:48:55 +0900

    Fix: Reservation과 Battery 엔티티 매핑

--- backend/src/main/java/backend/domain/battery/dto/BatteryDto.java
+++ backend/src/main/java/backend/domain/battery/dto/BatteryDto.java
@@ -1,5 +1,7 @@
 package backend.domain.battery.dto;
 
+import backend.domain.battery.entity.Battery;
+import backend.domain.battery.entity.Reservation;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +11,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BatteryDto {
     @Getter
@@ -46,8 +50,44 @@ public class BatteryDto {
         private boolean status;
         private int price;
         private String photoURL;
-        private long zoneId;
+//        private long zoneId;
         private LocalDateTime createdAt;
         private LocalDateTime modifiedAt;
+        private List<BatteryReservation> reservations;
+
+        public Response(Battery battery){
+            this.batteryId = battery.getBatteryId();
+            this.capacity = battery.getCapacity();
+            this.status = battery.isStatus();
+            this.price = battery.getPrice();
+            this.photoURL = battery.getPhotoURL();
+            this.createdAt = battery.getCreatedAt();
+            this.modifiedAt = battery.getModifiedAt();
+            this.reservations = BatteryReservation.batteryReservation(battery.getReservations());
+        }
+
+        @Getter
+        public static class BatteryReservation{
+            private Long reservationId;
+            private String startTime;
+            private String endTime;
+            private LocalDateTime reservationCreatedAt;
+            private LocalDateTime reservationModifiedAt;
+
+            public static List<BatteryReservation> batteryReservation(List<Reservation> reservations){
+                List<BatteryReservation> list = new ArrayList<>();
+                reservations.forEach(reservation -> {
+                    list.add(new BatteryReservation(reservation));
+                });
+                return list;
+            }
+            public BatteryReservation(Reservation reservation){
+                this.reservationId = reservation.getReservationId();
+                this.startTime = reservation.getStartTime();
+                this.endTime = reservation.getEndTime();
+                this.reservationCreatedAt = reservation.getCreatedAt();
+                this.reservationModifiedAt = reservation.getModifiedAt();
+            }
+        }
     }
 }
