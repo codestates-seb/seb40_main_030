d85622794c43058cc93a164dd1de73921401a033
Dongjin Kim <nyong9221@gmail.com>
2022-11-22 14:07:14 +0900

    Fix: Payment, Station 유효성 검증 추가

--- backend/src/main/java/backend/domain/station/dto/StationPostReqDto.java
+++ backend/src/main/java/backend/domain/station/dto/StationPostReqDto.java
@@ -5,6 +5,7 @@ import backend.domain.station.entity.StationLocation;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
@@ -12,9 +13,12 @@ import java.util.ArrayList;
 @Setter
 public class StationPostReqDto {
 
+    @NotBlank(message = "대여소 이름은 필수입니다.")
     private String name;
+    @NotBlank(message = "상세정보는 필수입니다.")
     private String details;
     private StationLocation location;
+    @NotBlank(message = "이미지는 필수입니다.")
     private String photoURL;
 
     public Station toStation() {
