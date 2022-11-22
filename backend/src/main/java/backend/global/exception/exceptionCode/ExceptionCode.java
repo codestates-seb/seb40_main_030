89f84b56af854b178d5f3395b90e902c70787164
Dongjin Kim <nyong9221@gmail.com>
2022-11-22 14:03:26 +0900

    Fix: EsceptionCode 정리 및 이름 수정

--- backend/src/main/java/backend/global/exception/exceptionCode/ExceptionCode.java
+++ backend/src/main/java/backend/global/exception/exceptionCode/ExceptionCode.java
@@ -3,24 +3,22 @@ package backend.global.exception.exceptionCode;
 import lombok.Getter;
 
 public enum ExceptionCode {
-    // 비즈니스 모델에서 필요시 에러코드 추가하는 부분
-
-    NOT_FOUND(404, "요청하신 데이터를 찾을 수 없습니다."),
-    NOT_FOUND_MEMBER(400, "계정을 찾을 수 없습니다."),
-
-    ACCOUNT_EXIST(400, "존재하는 계정입니다."),
 
     NON_ACCESS_MODIFY(401, "수정권한이 없습니다."),
+
     ACCESS_TOKEN_EXPIRATION(401, "재 로그인이 필요합니다."),
     LOGIN_FAILURE(401, "아이디 혹은 비밀번호가 옳지 않습니다."),
 
-    ILLEGAL_FILENAME(400, "잘못된 형식의 파일명입니다."),
-    BATTERY_NOT_FOUND(400, "해당하는 ID의 배터리가 존재하지 않습니다."),
-    ADMIN_NOT_FOUND(400, "해당하는 ID의 관리자가 존재하지 않습니다."),
-    ADMIN_EXIST(409, "해당 email의 관리자가 존재합니다."),
-    EMAIL_EXIST(401, "중복된 email 입니다."),
-    NICKNAME_EXIST(401, "중복된 nickname 입니다."),
-    NOT_FOUND_STATION(400, "해당하는 ID의 대여소가 존재하지 않습니다.");
+    ADMIN_EXIST(401, "중복된 관리자 계정입니다."),
+    EMAIL_EXIST(401, "중복된 이메일입니다."),
+    NICKNAME_EXIST(401, "중복된 닉네임입니다."),
+
+    NOT_FOUND(404, "요청하신 데이터를 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(404, "해당 계정이 존재하지 않습니다."),
+    BATTERY_NOT_FOUND(404, "해당 배터리가 존재하지 않습니다."),
+    ADMIN_NOT_FOUND(404, "해당 관리자가 존재하지 않습니다."),
+    STATION_NOT_FOUND(404, "해당 대여소가 존재하지 않습니다."),
+    PAY_NOT_FOUND(404, "해당 결제가 존재하지 않습니다.");
 
 
     @Getter
