23d7be427545dbca60548be6826b3612e9b3d9b9
mingu0429 <mingu0429@gmail.com>
2022-11-23 12:24:46 +0900

    Fix: Mock 데이터 수정

--- backend/src/main/resources/static/db/data.sql
+++ backend/src/main/resources/static/db/data.sql
@@ -3,19 +3,19 @@ INSERT INTO Member(member_id,createdAt,modifiedAt,address,email,nickname,passwor
 INSERT INTO Member(member_id,createdAt,modifiedAt,address,email,nickname,password,phone,photoURL) VALUES (2,'2022-11-19 17:33:04.000228','2022-11-19 17:33:04.000228','서울시 강남구 역삼동','nike@gmail.com','나이키','123411aa','010-1111-2222','http://asd311114f6as3d54f6aw');
 
 --더미 Admin
-INSERT INTO Admin(adminId,createdAt,email,modifiedAt,password,phone) VALUES (1,'2022-11-19 17:33:16.026388','admin@gmail.com','2022-11-19 17:33:16.026388','asdf5t1234*','010-7942-7942');
-INSERT INTO Admin(adminId,createdAt,email,modifiedAt,password,phone) VALUES (2,'2022-11-19 17:33:38.289305','admin2@gmail.com','2022-11-19 17:33:38.290304','asdf5t12342*','010-7942-7942');
+INSERT INTO Admin(admin_id,createdAt,email,modifiedAt,password,phone) VALUES (1,'2022-11-19 17:33:16.026388','admin@gmail.com','2022-11-19 17:33:16.026388','asdf5t1234*','010-7942-7942');
+INSERT INTO Admin(admin_id,createdAt,email,modifiedAt,password,phone) VALUES (2,'2022-11-19 17:33:38.289305','admin2@gmail.com','2022-11-19 17:33:38.290304','asdf5t12342*','010-7942-7942');
 
 -- 더미 Station
-INSERT INTO Station(station_id,createdAt,modifiedAt,details,latitude,longitude,name,photoURL,phone) VALUES (1,'2022-11-19 17:33:53.258093','2022-11-19 17:33:53.258093','첫번째 대여소',37.53737528,127.00557633,'인천제일주유소','@#!@DSAF!@#','010-1588-1588');
-INSERT INTO Station(station_id,createdAt,modifiedAt,details,latitude,longitude,name,photoURL,phone) VALUES (2,'2022-11-19 17:34:05.641588','2022-11-19 17:34:05.641588','두번째 대여소',37.545024,127.03923,'서울제일주유소','@#!@DSAF!@#','010-2580-2580');
+INSERT INTO Station(station_id, admin_id, createdAt,modifiedAt,details,latitude,longitude,name,photoURL,phone) VALUES (1,"1",'2022-11-19 17:33:53.258093','2022-11-19 17:33:53.258093','첫번째 대여소',37.53737528,127.00557633,'인천제일주유소','@#!@DSAF!@#','010-1588-1588');
+INSERT INTO Station(station_id, admin_id, createdAt,modifiedAt,details,latitude,longitude,name,photoURL,phone) VALUES (2,"2",'2022-11-19 17:34:05.641588','2022-11-19 17:34:05.641588','두번째 대여소',37.545024,127.03923,'서울제일주유소','@#!@DSAF!@#','010-2580-2580');
 
 --더미 Battery
-INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id) VALUES (1,'100000mA','2022-11-19 17:34:17.716045','2022-11-19 17:34:17.716045','http://asdfqwer111',50000,false,1);
-INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id) VALUES (2,'100000mA','2022-11-19 17:34:18.453315','2022-11-19 17:34:18.453315','http://asdfqwer111',50000,false,1);
-INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id) VALUES (3,'100000mA','2022-11-19 17:34:19.139197','2022-11-19 17:34:19.139197','http://asdfqwer111',50000,false,1);
-INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id) VALUES (4,'100000mA','2022-11-19 17:34:22.887204','2022-11-19 17:34:22.887204','http://asdfqwer111',50000,true,2);
-INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id) VALUES (5,'100000mA','2022-11-19 17:34:23.503853','2022-11-19 17:34:23.503853','http://asdfqwer111',50000,true,2);
+INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id, batteryName) VALUES (1,'100000mA','2022-11-19 17:34:17.716045','2022-11-19 17:34:17.716045','http://asdfqwer111',50000,false,1,"Samsung");
+INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id, batteryName) VALUES (2,'100000mA','2022-11-19 17:34:18.453315','2022-11-19 17:34:18.453315','http://asdfqwer111',50000,false,1,"Samsung");
+INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id, batteryName) VALUES (3,'100000mA','2022-11-19 17:34:19.139197','2022-11-19 17:34:19.139197','http://asdfqwer111',50000,false,1,"Samsung");
+INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id, batteryName) VALUES (4,'100000mA','2022-11-19 17:34:22.887204','2022-11-19 17:34:22.887204','http://asdfqwer111',50000,true,2,"Hyundai");
+INSERT INTO Battery(batteryId,capacity,createdAt,LAST_MODIFIED_AT,photoURL,price,status,station_id, batteryName) VALUES (5,'100000mA','2022-11-19 17:34:23.503853','2022-11-19 17:34:23.503853','http://asdfqwer111',50000,true,2,"Hyundai");
 
 -- 더미 payment
 INSERT INTO Payment(id,createdAt,modifiedAt,endTime,payMethod,startTime,status,totalPrice,battery_id,member_id,station_id) VALUES (1,'2022-11-19 17:34:32.536094','2022-11-19 17:34:32.537094','2022-11-20T23:30','카카오페이','2022-11-20T17:30',0,50000,1,1,1);
