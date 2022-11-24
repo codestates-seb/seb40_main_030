d9d48e6b89752360805107d1c63c6a41da01c6a5
leezer94 <2kunhee94@gmail.com>
2022-11-24 13:34:41 +0900

    Change : PrivateRouter 제거

--- frontend/src/pages/Rental/index.jsx
+++ frontend/src/pages/Rental/index.jsx
@@ -1,25 +1,18 @@
 import { useParams } from 'react-router-dom';
 
 import { PageWrapper } from '@/components/@commons';
-import { PrivateRouter } from '@/components/@helper';
 import RentalStatus from '@/components/Rental/RentalStatus';
-import { ROUTES } from '@/constants';
 import { useCheckDateFixed } from '@/hooks';
 
 const Rental = () => {
   const { stationId } = useParams();
   const { isDateFixed } = useCheckDateFixed();
+  isDateFixed;
 
   return (
-    <PrivateRouter
-      when={isDateFixed}
-      message='예약시간 설정을 해주세요'
-      path={ROUTES.HOME.PATH}
-    >
-      <PageWrapper title='배터리 보유 현황' path={-1}>
-        <RentalStatus id={stationId} />
-      </PageWrapper>
-    </PrivateRouter>
+    <PageWrapper title='배터리 보유 현황' path={-1}>
+      <RentalStatus id={stationId} />
+    </PageWrapper>
   );
 };
 
