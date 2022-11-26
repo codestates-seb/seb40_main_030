45b8b823231150b12c5f25b6eb605611877a7808
leezer94 <2kunhee94@gmail.com>
2022-11-26 16:43:33 +0900

    Design : theme 상수값 추가

---
+++ frontend/src/styles/size.js
@@ -0,0 +1,6 @@
+const SIZE = {
+  CIRCLE_SIZE: `clamp(1.5rem, 5vw, 3rem)`,
+  SPACING: `clamp(0.25rem, 2vw, 0.5rem)`,
+};
+
+export default SIZE;
--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -1,10 +1,12 @@
 // 공통 theme 요소
 
-import {COLOR, SIGNATURE_COLOR} from './color';
+import { COLOR, SIGNATURE_COLOR } from './color';
+import SIZE from './size';
 
 const theme = {
   ...COLOR,
-  ...SIGNATURE_COLOR
+  ...SIGNATURE_COLOR,
+  ...SIZE,
 };
 
 export default theme;
