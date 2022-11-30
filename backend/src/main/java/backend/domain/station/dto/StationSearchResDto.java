b61ba66fd4031aa805907b9876722df1b9d98b23
Dongjin Kim <nyong9221@gmail.com>
2022-12-01 03:27:41 +0900

    Fix: Station Search API 응답 DTO 구조 변경

--- backend/src/main/java/backend/domain/station/dto/StationSearchResDto.java
+++ backend/src/main/java/backend/domain/station/dto/StationSearchResDto.java
@@ -1,40 +1,47 @@
 package backend.domain.station.dto;
 
+import backend.domain.battery.entity.Battery;
+import backend.domain.station.entity.StationLocation;
 import backend.domain.station.entity.StationSearch;
 import backend.global.auditing.BaseTime;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 
 @Getter @Setter
 public class StationSearchResDto extends BaseTime {
 
-    private Long stationId;
-    private String stationName;
+    private Long id;
+    private String name;
     private String details;
-    private double latitude;
-    private double longitude;
+    private StationLocation location;
     private String photoURL;
     private String phone;
     private Integer confirmId;
     private int availableBatteryCount = 0;
     private String startTime;
     private String endTime;
+    private List<Battery> batteries;
 
     public StationSearchResDto(StationSearch stationSearch) {
-        this.stationId = stationSearch.getId();
-        this.stationName = stationSearch.getName();
+        setCreatedAt(stationSearch.getCreatedAt());
+        setModifiedAt(stationSearch.getModifiedAt());
+        this.id = stationSearch.getId();
+        this.name = stationSearch.getName();
         this.details = stationSearch.getDetails();
-        this.latitude = stationSearch.getLatitude();
-        this.longitude = stationSearch.getLongitude();
+        StationLocation location = new StationLocation();
+        location.setLatitude(stationSearch.getLatitude());
+        location.setLatitude(stationSearch.getLongitude());
+        this.location = location;
         this.photoURL = stationSearch.getPhotoURL();
         this.phone = stationSearch.getPhone();
         this.confirmId = stationSearch.getConfirmId();
         this.availableBatteryCount = stationSearch.getCount();
         this.startTime = stationSearch.getStartTime();
         this.endTime = stationSearch.getEndTime();
-        setCreatedAt(stationSearch.getCreatedAt());
-        setModifiedAt(stationSearch.getModifiedAt());
+        this.batteries = stationSearch.getBatteryList();
     }
 
 }
