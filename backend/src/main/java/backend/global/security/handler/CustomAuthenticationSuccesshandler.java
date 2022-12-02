6d1018ea1272159c406a778c723e1609eee0edc9
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-02 20:04:20 +0900

    [BE] 더미 API (결제 상태 강제 변환 + 더미 reservation생성) (#144)
    Feat: 더미 API (결제 상태 강제 변환 + 더미 reservation생성)
    Feat: 로그인 API 안내 메세지 반환
    Fix : 카카오 페이 controller 강제 Redirect
    Fix: 더미 데이터 수정 (Payments)

--- backend/src/main/java/backend/global/security/handler/CustomAuthenticationSuccesshandler.java
+++ backend/src/main/java/backend/global/security/handler/CustomAuthenticationSuccesshandler.java
@@ -1,5 +1,7 @@
 package backend.global.security.handler;
 
+import backend.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -8,14 +10,32 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-@Slf4j
+@Slf4j @RequiredArgsConstructor
 public class CustomAuthenticationSuccesshandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
         log.info("success login");
+        Member member = (Member) authentication.getPrincipal();
+
+        // 더미 데이터
+        List<String> adminMailAddress = new ArrayList<>();
+        adminMailAddress.add("admin@gmail.com");
+        adminMailAddress.add("admin1@gmail.com");
+        adminMailAddress.add("admin2@gmail.com");
+        adminMailAddress.add("admin3@gmail.com");
+
+        if(adminMailAddress.contains(member.getEmail())) {
+            response.getWriter().print("Success Login : ADMIN");
+        } else {
+            response.getWriter().print("Success Login : USER");
+        }
+
+        response.getWriter().flush();
     }
 
 }
