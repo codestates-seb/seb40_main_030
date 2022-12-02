915d43f2c1dffe65c433b7c0ebdb105f55adf2d9
leezer94 <2kunhee94@gmail.com>
2022-12-02 22:04:00 +0900

    Style : 코드 스타일 수정

--- frontend/src/components/Orders/Features/Extends/Extends.jsx
+++ frontend/src/components/Orders/Features/Extends/Extends.jsx
@@ -9,26 +9,28 @@ const Extends = ({ endTime, paymentsId }) => {
   const { data } = useGetAvailableExtendPeriod(paymentsId);
   const { possibleEndTime } = data;
 
-  const timeDifferenceInHour =
-    (new Date(possibleEndTime).getTime() - new Date(endTime).getTime()) /
-    (1000 * 60 * 60);
+  if (possibleEndTime) {
+    const timeDifferenceInHour =
+      (new Date(possibleEndTime)?.getTime() - new Date(endTime)?.getTime()) /
+      (1000 * 60 * 60);
 
-  return (
-    <S.ContentWrapper>
-      <span className='max-extend-time'>
-        최대 연장가능 시간은{' '}
-        {timeDifferenceInHour >= 24 ? 24 : timeDifferenceInHour}시간 입니다.
-      </span>
-      <ModalHeader title='배터리 대여기간 연장하기' />
-      <DateBox endTime={endTime} fontSize='20px' />
-      <S.DateSelectContainer>
-        <Counter min={0} max={23} range={1} />
-        <Counter min={0} max={50} range={10} />
-      </S.DateSelectContainer>
-      {/* 이부분은 반납 클릭했을때 바뀌도록 */}
-      <S.ExtendedDate>예정 반납 시간</S.ExtendedDate>
-      <DateBox endTime={'2022-12-03T20:30'} fontSize='20px' />
-    </S.ContentWrapper>
-  );
+    return (
+      <S.ContentWrapper>
+        <span className='max-extend-time'>
+          최대 연장가능 시간은{' '}
+          {timeDifferenceInHour >= 24 ? 24 : timeDifferenceInHour}시간 입니다.
+        </span>
+        <ModalHeader title='배터리 대여기간 연장하기' />
+        <DateBox endTime={endTime} fontSize='20px' />
+        <S.DateSelectContainer>
+          <Counter min={0} max={23} range={1} />
+          <Counter min={0} max={50} range={10} />
+        </S.DateSelectContainer>
+        {/* 이부분은 반납 클릭했을때 바뀌도록 */}
+        <S.ExtendedDate>예정 반납 시간</S.ExtendedDate>
+        <DateBox endTime={'2022-12-03T20:30'} fontSize='20px' />
+      </S.ContentWrapper>
+    );
+  }
 };
 export default Extends;
