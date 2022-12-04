caa292e1c15cd88a461281c04b5947c953e0e0b0
22ehd22 <107889751+22ehd22@users.noreply.github.com>
2022-12-04 19:48:25 +0900

    [BE] feat : oauth 회원가입 및 로그인 (#167)

--- backend/src/main/java/backend/global/security/oauth/KakaoController.java
+++ backend/src/main/java/backend/global/security/oauth/KakaoController.java
@@ -3,6 +3,7 @@ package backend.global.security.oauth;
 import backend.domain.member.dto.MemberDto;
 import backend.domain.member.entity.Member;
 import backend.domain.member.service.MemberService;
+import backend.global.security.filter.JwtAuthenticationFilter;
 import backend.global.security.jwt.JwtTokenizer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -10,10 +11,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
@@ -23,6 +25,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -61,13 +68,13 @@ public class KakaoController {
 //        return mav;
 //    }
 
-
-    private KakaoOauthService kakaoOauthService;
-
-    private final JwtTokenizer jwtTokenizer;
+    @Value("${cos.key}")
+    private String coskey;
+    private final MemberService memberService;
+//    private KakaoOauthService kakaoOauthService;
 
     @GetMapping("/auth/login2")
-    public String kakaoLogin(String code) {
+    public ModelAndView kakaoLogin(String code) {
 
         RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -75,7 +82,7 @@ public class KakaoController {
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
-        params.add("client_id", "a1690b88b9c76826da040f8aadd2da65");
+        params.add("client_id", "a6f21f427f37b9bdfd08bdcf65ef3faf");
         params.add("redirect_url", "http://localhost:8080/auth/login2");
         params.add("code", code);
 
@@ -102,8 +109,6 @@ public class KakaoController {
 
         RestTemplate rt2 = new RestTemplate();
         HttpHeaders headers2 = new HttpHeaders();
-
-//
         headers2.add("Authorization", "Bearer "+   kakaoToken.getAccess_token());
         headers2.add("Contet-type", "application/x-www-form-urlencoded;charset=utf-8");
 
@@ -128,11 +133,87 @@ public class KakaoController {
             e.printStackTrace();
         }
 
-        kakaoOauthService.loginKakao(kakaoToken, kakaoProfile); //nullPointException 가능성있음
+//        kakaoOauthService.loginKakao(kakaoToken, kakaoProfile); //nullPointException 가능성있음
 
 //        headers2.set("Authorization",jwtTokenizer.delegateAccessToken(member));
+        Member kakaoMember = Member.builder()
+                .id(kakaoProfile.getId())
+                .nickname(kakaoProfile.getKakao_account().getProfile().getNickname())
+                .password(coskey)
+                .email(kakaoProfile.getKakao_account().getEmail())
+                .phone("null")
+                .address("null")
+                .detailAddress("null")
+                .photoURL("http://null")
+                .build();
 
 
-        return response.getBody()+","+response2.getBody();
+        Member originmember = memberService.find(kakaoMember.getNickname());
+
+        if(originmember == null) {
+            memberService.create(kakaoMember);
+        }
+
+        //로그인
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(kakaoMember.getEmail(), coskey);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+//        HttpHeaders headers1 = new HttpHeaders();
+//        headers1.setLocation(URI.create("/"));
+//        return new ResponseEntity<>(headers1, HttpStatus.MOVED_PERMANENTLY);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("userId", kakaoProfile.getKakao_account().email);
+        mav.setViewName("index");
+        return mav;
+
+//        memberService.createMember(kakaoMember);
+
+//        return "redirect:/";
+//        Member kakaoMember = new Member();
+//        kakaoMember.setId(kakaoProfile.getId());
+////        kakaoMember.setEmail(kakaoProfile.getKakao_account().getEmail());
+//        kakaoMember.setEmail(kakaoProfile.getId() + "@naver.com");
+//        kakaoMember.setPassword("123411aa");
+////        kakaoMember.setNickname(kakaoProfile.getProperties().getNickname()); // property == null
+//        kakaoMember.setNickname("먹는샘물" + kakaoProfile.getId());
+//        kakaoMember.setKakaoAccessToken(kakaoToken.getAccess_token());
+//        kakaoMember.setKakaoRefreshToken(kakaoToken.getRefresh_token());
+//
+//        return memberService.createMember(kakaoMember);
+    }
+    @RequestMapping(value="/logout")
+    public ModelAndView logout(HttpSession session) {
+        ModelAndView mav = new ModelAndView();
+
+        kakaoLogout((String)session.getAttribute("accessToken"));
+        session.removeAttribute("accessToken");
+        session.removeAttribute("userId");
+        mav.setViewName("index");
+        return mav;
+    }
+    public void kakaoLogout(String accessToken) {
+        String reqURL = "https://kapi.kakao.com/v1/user/logout";
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("post");
+            conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+            int responseCode = conn.getResponseCode();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String result = "";
+            String line = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
--- backend/src/main/java/backend/global/security/oauth/KakaoOauthService.java
+++ backend/src/main/java/backend/global/security/oauth/KakaoOauthService.java
@@ -1,52 +1,52 @@
-package backend.global.security.oauth;
-
-import java.time.LocalDateTime;
-import java.util.*;
-import backend.domain.member.dto.MemberDto;
-import backend.domain.member.entity.Member;
-import backend.domain.member.repository.MemberRepository;
-import backend.domain.member.service.MemberService;
-import backend.global.exception.dto.BusinessLogicException;
-import backend.global.exception.exceptionCode.ExceptionCode;
-import backend.global.security.jwt.JwtTokenizer;
-import backend.global.security.utils.CustomAuthorityUtils;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Slf4j
-@Transactional
-@RequiredArgsConstructor
-public class KakaoOauthService {
-
-    private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final CustomAuthorityUtils customAuthorityUtils;
-    private final MemberService memberService;
-
-
-    public Member loginKakao(KakaoToken kakaoToken, KakaoProfile kakaoProfile) {
-
-        Member kakaoMember = new Member();
-        kakaoMember.setId(kakaoProfile.getId());
-//        kakaoMember.setEmail(kakaoProfile.getKakao_account().getEmail());
-        kakaoMember.setEmail(kakaoProfile.getId() + "@naver.com");
-        kakaoMember.setPassword("123411aa");
-//        kakaoMember.setNickname(kakaoProfile.getProperties().getNickname()); // property == null
-        kakaoMember.setNickname("먹는샘물" + kakaoProfile.getId());
-        kakaoMember.setKakaoAccessToken(kakaoToken.getAccess_token());
-        kakaoMember.setKakaoRefreshToken(kakaoToken.getRefresh_token());
-
-        return memberRepository.save(kakaoMember);
-
-//        return memberService.createMember(kakaoMember);  // 얘랑 이 밑에 전체랑 같은 소리임.
-
-
-//        String encryptedPassword = passwordEncoder.encode(kakaoMember.getPassword());
+//package backend.global.security.oauth;
+//
+//import java.time.LocalDateTime;
+//import java.util.*;
+//import backend.domain.member.dto.MemberDto;
+//import backend.domain.member.entity.Member;
+//import backend.domain.member.repository.MemberRepository;
+//import backend.domain.member.service.MemberService;
+//import backend.global.exception.dto.BusinessLogicException;
+//import backend.global.exception.exceptionCode.ExceptionCode;
+//import backend.global.security.jwt.JwtTokenizer;
+//import backend.global.security.utils.CustomAuthorityUtils;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Service
+//@Slf4j
+//@Transactional
+//@RequiredArgsConstructor
+//public class KakaoOauthService {
+//
+//    private final MemberRepository memberRepository;
+//    private final PasswordEncoder passwordEncoder;
+//    private final CustomAuthorityUtils customAuthorityUtils;
+//    private final MemberService memberService;
+//
+//
+//    public Member loginKakao(KakaoToken kakaoToken, KakaoProfile kakaoProfile) {
+//
+//        Member kakaoMember = new Member();
+//        kakaoMember.setId(kakaoProfile.getId());
+////        kakaoMember.setEmail(kakaoProfile.getKakao_account().getEmail());
+//        kakaoMember.setEmail(kakaoProfile.getId() + "@naver.com");
+//        kakaoMember.setPassword("123411aa");
+////        kakaoMember.setNickname(kakaoProfile.getProperties().getNickname()); // property == null
+//        kakaoMember.setNickname("먹는샘물" + kakaoProfile.getId());
+//        kakaoMember.setKakaoAccessToken(kakaoToken.getAccess_token());
+//        kakaoMember.setKakaoRefreshToken(kakaoToken.getRefresh_token());
+//
+//        return memberRepository.save(kakaoMember);
+//
+////        return memberService.createMember(kakaoMember);  // 얘랑 이 밑에 전체랑 같은 소리임.
+//
+//
+////        String encryptedPassword = passwordEncoder.encode(kakaoMember.getPassword());
 //        kakaoMember.setPassword(encryptedPassword);
 //
 //        List<String> roles = customAuthorityUtils.createRoles(kakaoMember.getEmail());
@@ -61,8 +61,8 @@ public class KakaoOauthService {
 //        Optional<Member> optionalNickname = memberRepository.findByMemberNickname(kakaoMember.getNickname());
 //        if (optionalNickname.isPresent()) return;
 //
-//        memberRepository.save(kakaoMember);
-
-    }
-
-}
+////        memberRepository.save(kakaoMember);
+//
+//    }
+//
+//}
