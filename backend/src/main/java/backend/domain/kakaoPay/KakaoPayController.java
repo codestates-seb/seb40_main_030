a3de34f9c4522a12354cc875458a3ea6fdb65744
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-07 03:29:03 +0900

    [BE] 카카오 및 일부 API 수정 (#202)
    * [BE] Oauth API 수정완료, Kakao결제 API 수정, JwtExtractUtils에 Oauth용 로직 추가, Payment API 수정 (#195)
    
    * [BE] KakaoPay 리다이렉트 경로 수정 (#197)
    
    * [BE] Kakao API 들 수정 (#201)

--- backend/src/main/java/backend/domain/kakaoPay/KakaoPayController.java
+++ backend/src/main/java/backend/domain/kakaoPay/KakaoPayController.java
@@ -5,18 +5,21 @@ import backend.domain.payment.service.PaymentService;
 import backend.global.security.utils.JwtExtractUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import lombok.Setter;
 import lombok.extern.java.Log;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import javax.servlet.http.HttpServletRequest;
 
 @Log
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping
 public class KakaoPayController {
 
     @Setter(onMethod_ = @Autowired)
@@ -24,43 +27,58 @@ public class KakaoPayController {
     private final PaymentService paymentService;
     private final JwtExtractUtils jwtExtractUtils;
 
-    @PostMapping("/kakaoPay")
-    public String kakaoPay(@RequestParam(name = "itemName") String itemName,
-                           @RequestParam(name = "totalAmount") int totalAmount,
-                           @RequestParam(name = "batteryId") Long batteryId,
-                           @RequestParam(name = "startTime") String startTime,
-                           @RequestParam(name = "endTime") String endTime,
-                           HttpServletRequest request) {
+    @GetMapping("/kakaoPay")
+    public ResponseEntity kakaoPay(@RequestParam(required = false, name = "itemName") String itemName,
+                                                 @RequestParam(required = false, name = "totalAmount") int totalAmount,
+                                                 @RequestParam(required = false, name = "batteryId") Long batteryId,
+                                                 @RequestParam(required = false, name = "startTime") String startTime,
+                                                 @RequestParam(required = false, name = "endTime") String endTime,
+                                                 HttpServletRequest request) {
         log.info("kakaoPay post............................................");
-//        Long memberId = jwtExtractUtils.extractMemberIdFromJwt(request);
-        Long memberId= 1L;
+        Long memberId = jwtExtractUtils.extractMemberIdFromJwt(request);
         Payment payment = new Payment(startTime, endTime, totalAmount);
-        Payment payment2 = paymentService.postPayment(payment, batteryId, memberId);
-        Long paymentId = payment2.getId();
+        Payment savedPayment = paymentService.postPayment(payment, batteryId, memberId);
+        Long paymentId = savedPayment.getId();
+        KakaoPayReadyVO response = kakaopay.kakaoPayReady(itemName, totalAmount, paymentId);
 
-        return "redirect:" + kakaopay.kakaoPayReady(itemName, totalAmount, paymentId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @GetMapping("/kakaoPaySuccess")
-    public void kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
+
+    @GetMapping("/kakaoPaySuccess/{paymentId}")
+    public String kakaoPaySuccess(@RequestParam(required = false) String pg_token,
+                                  @PathVariable("paymentId") Long paymentId, RedirectAttributes redirectAttributes) {
         log.info("kakaoPaySuccess get............................................");
         log.info("kakaoPaySuccess pg_token : " + pg_token);
 
-        model.addAttribute("info", kakaopay.kakaoPayInfo(pg_token));
+        kakaopay.kakaoPayInfo(paymentId, pg_token);
+        redirectAttributes.addAttribute("paymentId", paymentId);
 
+        return "redirect:http://battery-bucket-deploy.s3-website.ap-northeast-2.amazonaws.com/payments/payment_completed";
     }
 
     // 결제 취소시 실행 url
     @PostMapping("/kakaoPayCancel")
     public void kakaoPayCancel(@RequestParam(name = "paymentId") Long paymentId,
-                               @RequestParam(name = "cancel_amount") int cancel_amount,Model model) {
-        model.addAttribute("info", kakaopay.kakaoPayCancel(paymentId, cancel_amount));
+                               Model model) {
+        model.addAttribute("info", kakaopay.kakaoPayCancel(paymentId));
     }
+
+    // 결제 취소시 실행 url
+//    @GetMapping("/kakaoPayCancel/{paymentId}")
+//    public String kakaoPayCancel(@PathVariable("paymentId") Long paymentId) {
+//
+//        kakaopay.kakaoPayCancelOrFail(paymentId);
+//        log.info("결제가 취소되었습니다.");
+//        return "redirect:http://battery-bucket-deploy.s3-website.ap-northeast-2.amazonaws.com/kakaoPayCancel";
+//    }
 
     // 결제 실패시 실행 url
-    @GetMapping("/kakaoPaySuccessFail")
-    public String payFail() {
-        return "redirect:/";
-    }
-
+//    @GetMapping("/kakaoPaySuccessFail/{paymentId}")
+//    public String kakaoPaySuccessFail(@PathVariable("paymentId") Long paymentId) {
+//
+//        kakaopay.kakaoPayCancelOrFail(paymentId);
+//        log.info("결제가 실패되었습니다.");
+//        return "redirect:http://battery-bucket-deploy.s3-website.ap-northeast-2.amazonaws.com/kakaoPaySuccessFail";
+//    }
 }
--- backend/src/main/java/backend/global/security/oauth/KakaoOauthService.java
+++ backend/src/main/java/backend/global/security/oauth/KakaoOauthService.java
@@ -1,19 +1,10 @@
 package backend.global.security.oauth;
 
-import java.time.LocalDateTime;
-import java.util.*;
-import backend.domain.member.dto.MemberDto;
+
 import backend.domain.member.entity.Member;
 import backend.domain.member.repository.MemberRepository;
-import backend.domain.member.service.MemberService;
-import backend.global.exception.dto.BusinessLogicException;
-import backend.global.exception.exceptionCode.ExceptionCode;
-import backend.global.security.jwt.JwtTokenizer;
-import backend.global.security.utils.CustomAuthorityUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,45 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class KakaoOauthService {
 
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final CustomAuthorityUtils customAuthorityUtils;
-    private final MemberService memberService;
-
 
     public Member loginKakao(KakaoToken kakaoToken, KakaoProfile kakaoProfile) {
 
-        Member kakaoMember = new Member();
-        kakaoMember.setId(kakaoProfile.getId());
-//        kakaoMember.setEmail(kakaoProfile.getKakao_account().getEmail());
-        kakaoMember.setEmail(kakaoProfile.getId() + "@naver.com");
-        kakaoMember.setPassword("123411aa");
-//        kakaoMember.setNickname(kakaoProfile.getProperties().getNickname()); // property == null
-        kakaoMember.setNickname("먹는샘물" + kakaoProfile.getId());
-        kakaoMember.setKakaoAccessToken(kakaoToken.getAccess_token());
-        kakaoMember.setKakaoRefreshToken(kakaoToken.getRefresh_token());
-
-        return memberRepository.save(kakaoMember);
-
-//        return memberService.createMember(kakaoMember);  // 얘랑 이 밑에 전체랑 같은 소리임.
-
-
-//        String encryptedPassword = passwordEncoder.encode(kakaoMember.getPassword());
-//        kakaoMember.setPassword(encryptedPassword);
-//
-//        List<String> roles = customAuthorityUtils.createRoles(kakaoMember.getEmail());
-//        kakaoMember.setRoles(roles);
-//
-//        kakaoMember.setCreatedAt(LocalDateTime.now());
-//        kakaoMember.setModifiedAt(LocalDateTime.now());
-//
-//        Optional<Member> optionalEmail = memberRepository.findByMemberEmail(kakaoMember.getEmail());
-//        if (optionalEmail.isPresent()) return;
-//
-//        Optional<Member> optionalNickname = memberRepository.findByMemberNickname(kakaoMember.getNickname());
-//        if (optionalNickname.isPresent()) return;
-//
-//        memberRepository.save(kakaoMember);
-
+        return null;
     }
 
 }
